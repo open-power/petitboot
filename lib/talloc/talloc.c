f5d6bf0af6aa5e6f1d51e00a617910ddfb76e32c
Geoff Levand <geoff@infradead.org>
2011-11-12 13:42:31 -0800

    Add va-copy check to configure.ac
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- lib/talloc/talloc.c
+++ lib/talloc/talloc.c
@@ -26,19 +26,20 @@
   inspired by http://swapped.cc/halloc/
 */
 
-
+#if defined(HAVE_CONFIG_H)
 #include "config.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#else
 /* nfsim additions */
 #define HAVE_SYS_TYPES_H
 #define HAVE_UNISTD_H
 #define HAVE_STDARG_H
 #define HAVE_STDINT_H
 #define HAVE_VA_COPY
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #ifdef HAVE_SYS_TYPES_H
 #include <sys/types.h>
