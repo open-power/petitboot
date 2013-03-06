bf40f5fbd759241d1505c985fa02441cd6febd64
Jeremy Kerr <jk@ozlabs.org>
2013-03-06 13:42:15 +0800

    test: Add URL parser test infrastructure
    This change adds some simple testing infrastrcture to the URL parser.
    We use a small C binary (parse-url) to run the url parser on its
    argument, and compare the output with an expected datafile.
    
    An initial test is included, to check the behaviour of URLs with
    multiple slashes between host and pathname. This test currently fails.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Geoff Levand <geoff@infradead.org>

---
+++ test/urls/parse-url.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ui/common/url.h>
+#include <log/log.h>
+
+int main(int argc, char **argv)
+{
+	struct pb_url *url;
+	FILE *null;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <URL>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	/* discard log output */
+	null = fopen("/dev/null", "w");
+	pb_log_set_stream(null);
+
+	url = pb_url_parse(NULL, argv[1]);
+	if (!url)
+		return EXIT_FAILURE;
+
+	printf("%s\n", argv[1]);
+	printf("scheme\t%s\n", pb_url_scheme_name(url->scheme));
+	printf("host\t%s\n", url->host);
+	printf("port\t%s\n", url->port);
+	printf("path\t%s\n", url->path);
+	printf("dir\t%s\n", url->dir);
+	printf("file\t%s\n", url->file);
+
+	return EXIT_SUCCESS;
+}
