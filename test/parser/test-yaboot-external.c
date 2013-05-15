00f86a253a2058ad292f81ba09684fde9ac00539
Jeremy Kerr <jk@ozlabs.org>
2013-05-15 16:58:38 +0800

    test/parser: Add yaboot test for external devices
    Add a yaboot test to add boot options for an external device, then
    hotplug that device.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

---
+++ test/parser/test-yaboot-external.c
@@ -0,0 +1,36 @@
+
+#include "parser-test.h"
+
+#if 0 /* PARSER_EMBEDDED_CONFIG */
+default=
+
+image=external:/vmlinux
+	label=linux
+	initrd=external:/initrd
+#endif
+
+void run_test(struct parser_test *test)
+{
+	struct discover_boot_option *opt;
+	struct discover_context *ctx;
+	struct discover_device *dev;
+
+	test_read_conf_embedded(test);
+	test_run_parser(test, "yaboot");
+
+	ctx = test->ctx;
+
+	check_boot_option_count(ctx, 1);
+
+	opt = get_boot_option(ctx, 0);
+
+	check_name(opt, "linux");
+	check_unresolved_resource(opt->boot_image);
+	check_unresolved_resource(opt->initrd);
+
+	dev = test_create_device(ctx, "external");
+	test_hotplug_device(test, dev);
+
+	check_resolved_local_resource(opt->boot_image, dev, "/vmlinux");
+	check_resolved_local_resource(opt->initrd, dev, "/initrd");
+}
