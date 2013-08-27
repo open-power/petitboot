50a98e73d4eed46e859368915a7800cc904b44f2
Neelesh Gupta <neelegup@linux.vnet.ibm.com>
2013-08-27 14:34:51 +0530

    test: add 'partition=directive' test for yaboot conf file
    Signed-off-by: Neelesh Gupta <neelegup@linux.vnet.ibm.com>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

---
+++ test/parser/test-yaboot-partition.c
@@ -0,0 +1,33 @@
+#include "parser-test.h"
+
+#if 0 /* PARSER_EMBEDDED_CONFIG */
+device=sda
+partition=2
+
+image=/vmlinux
+	label=linux
+#endif
+
+void run_test(struct parser_test *test)
+{
+	struct discover_boot_option *opt;
+	struct discover_context *ctx;
+	struct discover_device *dev;
+
+	test_read_conf_embedded(test);
+	test_run_parser(test, "yaboot");
+
+	ctx = test->ctx;
+
+	check_boot_option_count(ctx, 1);
+
+	opt = get_boot_option(ctx, 0);
+
+	check_name(opt, "linux");
+	check_unresolved_resource(opt->boot_image);
+
+	dev = test_create_device(ctx, "sda2");
+	test_hotplug_device(test, dev);
+
+	check_resolved_local_resource(opt->boot_image, dev, "/vmlinux");
+}
