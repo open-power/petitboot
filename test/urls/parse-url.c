d8811f2d9a77afe1969c1b49b3117617f40cadda
Jeremy Kerr <jk@ozlabs.org>
2013-09-26 11:45:39 +0800

    tests/urls: Run URL tests under valgrind
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- test/urls/parse-url.c
+++ test/urls/parse-url.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <talloc/talloc.h>
 #include <url/url.h>
 #include <log/log.h>
 
 int main(int argc, char **argv)
 {
 	struct pb_url *url;
+	void *ctx;
 
 	if (argc != 2 && argc != 3) {
 		fprintf(stderr, "Usage: %s <URL> [update]\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	url = pb_url_parse(NULL, argv[1]);
+	ctx = talloc_new(NULL);
+
+	url = pb_url_parse(ctx, argv[1]);
 	if (!url)
 		return EXIT_FAILURE;
 
@@ -22,8 +26,11 @@ int main(int argc, char **argv)
 		printf("%s\n", argv[1]);
 
 	} else {
+		struct pb_url *new_url;
 		printf("%s %s\n", argv[1], argv[2]);
-		url = pb_url_join(NULL, url, argv[2]);
+		new_url = pb_url_join(ctx, url, argv[2]);
+		talloc_free(url);
+		url = new_url;
 	}
 
 	printf("scheme\t%s\n", pb_url_scheme_name(url->scheme));
@@ -32,6 +39,8 @@ int main(int argc, char **argv)
 	printf("path\t%s\n", url->path);
 	printf("dir\t%s\n", url->dir);
 	printf("file\t%s\n", url->file);
+
+	talloc_free(ctx);
 
 	return EXIT_SUCCESS;
 }
