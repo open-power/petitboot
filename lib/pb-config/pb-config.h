5ac810ae1779fdd730637d146faf8a762bedd522
Jeremy Kerr <jk@ozlabs.org>
2013-10-18 10:06:02 +0800

    lib: Move config structure defitinitions to types.h
    We don't want to require the lib/pb-config interface in the UIs, just
    the structure defintions. This change moves them to the common types
    header.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- lib/pb-config/pb-config.h
+++ lib/pb-config/pb-config.h
@@ -6,44 +6,6 @@
 
 #include <types/types.h>
 
-#define HWADDR_SIZE	6
-
-struct interface_config {
-	uint8_t	hwaddr[HWADDR_SIZE];
-	bool	ignore;
-	enum {
-		CONFIG_METHOD_DHCP,
-		CONFIG_METHOD_STATIC,
-	} method;
-	union {
-		struct {
-		} dhcp_config;
-		struct {
-			char *address;
-			char *gateway;
-		} static_config;
-	};
-};
-
-struct network_config {
-	struct interface_config	**interfaces;
-	int			n_interfaces;
-	const char		**dns_servers;
-	int			n_dns_servers;
-};
-
-struct boot_priority {
-	enum device_type	type;
-};
-
-struct config {
-	bool			autoboot_enabled;
-	int			autoboot_timeout_sec;
-	struct network_config	network;
-	struct boot_priority	*boot_priorities;
-	int			n_boot_priorities;
-};
-
 
 int config_init(void *ctx);
 const struct config *config_get(void);
--- lib/types/types.h
+++ lib/types/types.h
@@ -73,5 +73,42 @@ struct system_info {
 	unsigned int		n_interfaces;
 };
 
+#define HWADDR_SIZE	6
+
+struct interface_config {
+	uint8_t	hwaddr[HWADDR_SIZE];
+	bool	ignore;
+	enum {
+		CONFIG_METHOD_DHCP,
+		CONFIG_METHOD_STATIC,
+	} method;
+	union {
+		struct {
+		} dhcp_config;
+		struct {
+			char *address;
+			char *gateway;
+		} static_config;
+	};
+};
+
+struct network_config {
+	struct interface_config	**interfaces;
+	int			n_interfaces;
+	const char		**dns_servers;
+	int			n_dns_servers;
+};
+
+struct boot_priority {
+	enum device_type	type;
+};
+
+struct config {
+	bool			autoboot_enabled;
+	int			autoboot_timeout_sec;
+	struct network_config	network;
+	struct boot_priority	*boot_priorities;
+	int			n_boot_priorities;
+};
 
 #endif /* _TYPES_H */
