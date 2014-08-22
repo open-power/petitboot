76435cb23ccab4489fe59c2b5cdcd06ac9752c12
Samuel Mendoza-Jonas <sam.mj@au1.ibm.com>
2014-08-22 11:24:43 +1000

    ui/ncurses: Update 'add-url' help message
    We should make it clear that we're looking for a PXE-style config file.
    
    Signed-off-by: Samuel Mendoza-Jonas <sam.mj@au1.ibm.com>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- ui/ncurses/nc-add-url-help.c
+++ ui/ncurses/nc-add-url-help.c
@@ -1,8 +1,8 @@
 #include "nc-helpscreen.h"
 
 struct help_text add_url_help_text = define_help_text("\
-Supply a valid URL here to retrieve a remote boot config file, \
-(eg: petitboot.conf) and parse it.\n\
+Supply a valid URL here to retrieve a remote pxe-boot config file and parse it.\
 \n\
-URLs are of the form 'scheme://host/path/to/petitboot.conf', \
-such as tftp://host/petitboot.conf or http://host/petitboot.conf");
+\n\
+URLs are of the form 'scheme://host/path/to/pxeconffile', \
+such as tftp://host/pxeconffile or http://host/pxeconffile");
