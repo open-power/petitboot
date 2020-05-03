open-power/petitboot

commit 863c609797ca6d556dd3a4586fc07a6f4a5472d4

Add ncurses UI menu
Add support for an ncurses UI menu object.

Signed-off-by: Geoff Levand <geoffrey.levand@am.sony.com>
Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

ui/ncurses/nc-menu.h
@@ -1,0 +1,130 @@
+/*
+ *  Copyright (C) 2009 Sony Computer Entertainment Inc.
+ *  Copyright 2009 Sony Corp.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#if !defined(_PB_NC_MENU_H)
+#define _PB_NC_MENU_H
+
+#include <assert.h>
+#include <menu.h>
+
+#include "log/log.h"
+#include "pb-protocol/pb-protocol.h"
+#include "nc-scr.h"
+
+struct pmenu;
+
+/**
+ * struct pmenu_item - Hold the state of a single menu item.
+ * @i_sig: Signature for callback type checking, should be pmenu_item_sig.
+ * @nci: The ncurses menu item instance for this item.
+ */
+
+struct pmenu_item {
+	enum pb_nc_sig i_sig;
+	ITEM *nci;
+	struct pmenu *pmenu;
+	void *data;
+	int (*on_edit)(struct pmenu_item *item);
+	int (*on_execute)(struct pmenu_item *item);
+};
+
+struct pmenu_item *pmenu_item_alloc(struct pmenu *menu);
+struct pmenu_item *pmenu_item_setup(struct pmenu *menu, struct pmenu_item *i,
+	unsigned int index, const char *name, const char *description);
+void pmenu_item_delete(struct pmenu_item *item);
+
+static inline struct pmenu_item *pmenu_item_from_arg(void *arg)
+{
+	struct pmenu_item *item = (struct pmenu_item *)arg;
+
+	assert(item->i_sig == pb_item_sig);
+	return item;
+}
+
+static inline struct pmenu_item *pmenu_item_init(struct pmenu *menu,
+	unsigned int index, const char *name, const char *description)
+{
+	return pmenu_item_setup(menu, pmenu_item_alloc(menu), index, name,
+		description);
+}
+
+/**
+ * struct pmenu - Data structure defining complete menu.
+ * @insert_pt: Index in nc item array.
+ * @ncm: The ncurses menu instance for this menu.
+ */
+
+struct pmenu {
+	struct nc_scr scr;
+	MENU *ncm;
+	ITEM **items;
+	unsigned int item_count;
+	unsigned int insert_pt;
+	int (*hot_key)(struct pmenu *menu, struct pmenu_item *item, int c);
+	void (*on_exit)(struct pmenu *menu);
+};
+
+struct pmenu *pmenu_init(void *ui_ctx, unsigned int item_count,
+	void (*on_exit)(struct pmenu *));
+int pmenu_setup(struct pmenu *menu);
+void pmenu_delete(struct pmenu *menu);
+unsigned int pmenu_grow(struct pmenu *menu, unsigned int count);
+int pmenu_remove(struct pmenu *menu, struct pmenu_item *item);
+struct pmenu_item *pmenu_find_selected(struct pmenu *menu);
+
+/* convenience routines */
+
+int pmenu_exit_cb(struct pmenu_item *item);
+
+static inline struct pmenu *pmenu_from_scr(struct nc_scr *scr)
+{
+	struct pmenu *pmenu;
+
+	assert(scr->sig == pb_pmenu_sig);
+	pmenu = (struct pmenu *)((char *)scr
+		- (size_t)&((struct pmenu *)0)->scr);
+	assert(pmenu->scr.sig == pb_pmenu_sig);
+
+	return pmenu;
+}
+
+/* debug routines */
+
+#if defined(DEBUG)
+enum {do_debug = 1};
+#else
+enum {do_debug = 0};
+#endif
+
+static inline void pmenu_dump_item(const ITEM *item)
+{
+	if (do_debug)
+		pb_log("%p %s\n", item, (item ? item->name.str : "(null)"));
+}
+
+static inline void pmenu_dump_items(ITEM *const *items, unsigned int count)
+{
+	unsigned int i;
+
+	if (do_debug)
+		for (i = 0; i < count; i++)
+			pb_log("%u: %p %s\n", i, items[i],
+				(items[i] ? items[i]->name.str : "(null)"));
+}
+
+#endif

rules.mk
@@ -50,7 +50,7 @@ discover_objs = discover/event.o discover/user-event.o discover/udev.o \
 # client objs
 ui_common_objs = ui/common/discover-client.o ui/common/loader.o \
 	ui/common/ui-system.o ui/common/url.o
-ncurses_objs =
+ncurses_objs = ui/ncurses/nc-scr.o ui/ncurses/nc-menu.o
 twin_objs = ui/twin/pb-twin.o
 
 # Makefiles

ui/ncurses/nc-menu.c
@@ -1,0 +1,358 @@
+/*
+ *  Copyright (C) 2009 Sony Computer Entertainment Inc.
+ *  Copyright 2009 Sony Corp.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define _GNU_SOURCE
+
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+
+#include "log/log.h"
+#include "talloc/talloc.h"
+#include "ui/common/ui-system.h"
+#include "nc-menu.h"
+
+/**
+ * pmenu_exit_cb - Callback helper that runs run menu.on_exit().
+ */
+
+int pmenu_exit_cb(struct pmenu_item *item)
+{
+	assert(item->pmenu->on_exit);
+	item->pmenu->on_exit(item->pmenu);
+	return 0;
+}
+
+/**
+ * pmenu_find_selected - Find the selected pmenu_item.
+ */
+
+struct pmenu_item *pmenu_find_selected(struct pmenu *menu)
+{
+	return pmenu_item_from_arg(item_userptr(current_item(menu->ncm)));
+}
+
+static int pmenu_post(struct nc_scr *scr)
+{
+	int result;
+	struct pmenu *menu = pmenu_from_scr(scr);
+
+	result = post_menu(menu->ncm);
+
+	nc_scr_frame_draw(scr);
+	redrawwin(menu->scr.main_ncw);
+	wrefresh(menu->scr.main_ncw);
+
+	return result;
+}
+
+static int pmenu_unpost(struct nc_scr *scr)
+{
+	return unpost_menu(pmenu_from_scr(scr)->ncm);
+}
+
+static void pmenu_resize(struct nc_scr *scr)
+{
+	/* FIXME: menus can't be resized, need to recreate here */
+	pmenu_unpost(scr);
+	pmenu_post(scr);
+}
+
+/**
+ * pmenu_item_init - Allocate and initialize a new pmenu_item instance.
+ *
+ * Returns a pointer the the initialized struct pmenu_item instance or NULL
+ * on error. The caller is responsible for calling talloc_free() for the
+ * returned instance.
+ */
+
+struct pmenu_item *pmenu_item_alloc(struct pmenu *menu)
+{
+	/* Items go with the menu, not the pointer array. */
+
+	struct pmenu_item *i = talloc_zero(menu, struct pmenu_item);
+
+	return i;
+}
+
+struct pmenu_item *pmenu_item_setup(struct pmenu *menu, struct pmenu_item *i,
+	unsigned int index, const char *name,
+	const char *description)
+{
+	assert(i);
+
+	if (!i)
+		return NULL;
+
+	i->i_sig = pb_item_sig;
+	i->pmenu = menu;
+	i->nci = new_item(name, description);
+
+	if (!i->nci) {
+		talloc_free(i);
+		return NULL;
+	}
+
+	set_item_userptr(i->nci, i);
+
+	menu->items[index] = i->nci;
+
+	return i;
+}
+
+/**
+ * pmenu_move_cursor - Move the cursor.
+ * @req: An ncurses request or char to send to menu_driver().
+ */
+
+static void pmenu_move_cursor(struct pmenu *menu, int req)
+{
+	menu_driver(menu->ncm, req);
+	wrefresh(menu->scr.main_ncw);
+}
+
+/**
+ * pmenu_process_key - Process a user keystroke.
+ */
+
+static void pmenu_process_key(struct nc_scr *scr)
+{
+	struct pmenu *menu = pmenu_from_scr(scr);
+	struct pmenu_item *item = pmenu_find_selected(menu);
+
+	nc_scr_status_free(&menu->scr);
+
+	while (1) {
+		int c = getch();
+
+		if (c == ERR)
+			return;
+
+		/* DBGS("%d (%o)\n", c, c); */
+
+		if (menu->hot_key)
+			c = menu->hot_key(menu, item, c);
+
+		switch (c) {
+		case 27: /* ESC */
+			if (menu->on_exit)
+				menu->on_exit(menu);
+			nc_flush_keys();
+			return;
+
+		case KEY_PPAGE:
+			pmenu_move_cursor(menu, REQ_SCR_UPAGE);
+			break;
+		case KEY_NPAGE:
+			pmenu_move_cursor(menu, REQ_SCR_DPAGE);
+			break;
+		case KEY_HOME:
+			pmenu_move_cursor(menu, REQ_FIRST_ITEM);
+			break;
+		case KEY_END:
+			pmenu_move_cursor(menu, REQ_LAST_ITEM);
+			break;
+		case KEY_UP:
+			pmenu_move_cursor(menu, REQ_UP_ITEM);
+			break;
+		case KEY_DOWN:
+		case '\t':
+			pmenu_move_cursor(menu, REQ_DOWN_ITEM);
+			break;
+
+		case KEY_LEFT:
+		case 'E':
+		case 'e':
+			if (item->on_edit)
+				item->on_edit(item);
+			break;
+		case '\n':
+		case '\r':
+			if (item->on_execute)
+				item->on_execute(item);
+			break;
+		default:
+			menu_driver(menu->ncm, c);
+			break;
+		}
+	}
+}
+
+/**
+ * pmenu_grow - Grow the item array.
+ * @count: The count of new items.
+ *
+ * The item array must be disconnected prior to calling pmenu_grow().
+ * Returns the insert point index.
+ */
+
+unsigned int pmenu_grow(struct pmenu *menu, unsigned int count)
+{
+	unsigned int tmp;
+
+	assert(item_count(menu->ncm) == 0 && "not disconnected");
+
+	pb_log("%s: %u current + %u new = %u\n", __func__, menu->item_count,
+		count, menu->item_count + count);
+
+	/* Note that items array has a null terminator. */
+
+	menu->items = talloc_realloc(menu, menu->items, ITEM *,
+		menu->item_count + count + 1);
+
+	memmove(menu->items + menu->insert_pt + count,
+		menu->items + menu->insert_pt,
+		(menu->item_count - menu->insert_pt + 1) * sizeof(ITEM *));
+
+	memset(menu->items + menu->insert_pt, 0, count * sizeof(ITEM *));
+
+	tmp = menu->insert_pt;
+	menu->insert_pt += count;
+	menu->item_count += count;
+
+	return tmp;
+}
+
+static int pmenu_item_get_index(const struct pmenu_item *item)
+{
+	unsigned int i;
+
+	for (i = 0; i < item->pmenu->item_count; i++)
+		if (item->pmenu->items[i] == item->nci)
+			return i;
+
+	pb_log("%s: not found: %p %s\n", __func__, item,
+		(item ? item->nci->name.str : "(null)"));
+	return -1;
+}
+
+/**
+ * pmenu_remove - Remove an item from the item array.
+ *
+ * The item array must be disconnected prior to calling pmenu_remove()
+ */
+
+int pmenu_remove(struct pmenu *menu, struct pmenu_item *item)
+{
+	int index;
+
+	assert(item_count(menu->ncm) == 0 && "not disconnected");
+
+	assert(menu->item_count);
+
+	pb_log("%s: %u\n", __func__, menu->item_count);
+
+	index = pmenu_item_get_index(item);
+
+	if (index < 0)
+		return -1;
+
+	/* Note that items array has a null terminator. */
+
+	menu->insert_pt--;
+	menu->item_count--;
+
+	memmove(&menu->items[index], &menu->items[index + 1],
+		(menu->item_count - index + 1) * sizeof(ITEM *));
+	menu->items = talloc_realloc(menu, menu->items, ITEM *,
+		menu->item_count + 1);
+
+	return 0;
+}
+
+/**
+ * pmenu_init - Allocate and initialize a new menu instance.
+ *
+ * Returns a pointer the the initialized struct pmenu instance or NULL on error.
+ * The caller is responsible for calling talloc_free() for the returned
+ * instance.
+ */
+
+struct pmenu *pmenu_init(void *ui_ctx, unsigned int item_count,
+	void (*on_exit)(struct pmenu *))
+{
+	struct pmenu *menu = talloc_zero(ui_ctx, struct pmenu);
+
+	if (!menu)
+		return NULL;
+
+	/* note items array has a null terminator */
+
+	menu->items = talloc_zero_array(menu, ITEM *, item_count + 1);
+
+	if (!menu->items) {
+		talloc_free(menu);
+		return NULL;
+	}
+
+	nc_scr_init(&menu->scr, pb_pmenu_sig, 0, ui_ctx, pmenu_process_key,
+		pmenu_post, pmenu_unpost, pmenu_resize);
+
+	menu->item_count = item_count;
+	menu->insert_pt = 0; /* insert from top */
+	menu->on_exit = on_exit;
+
+	return menu;
+}
+
+/**
+ * pmenu_setup - Create nc menu, setup nc windows.
+ *
+ */
+
+int pmenu_setup(struct pmenu *menu)
+{
+	assert(!menu->ncm);
+
+	menu->ncm = new_menu(menu->items);
+
+	if (!menu->ncm) {
+		pb_log("%s:%d: new_menu failed: %s\n", __func__, __LINE__,
+			strerror(errno));
+		return -1;
+	}
+
+	set_menu_win(menu->ncm, menu->scr.main_ncw);
+	set_menu_sub(menu->ncm, menu->scr.sub_ncw);
+
+	/* Makes menu scrollable. */
+	set_menu_format(menu->ncm, LINES - nc_scr_frame_lines, 1);
+
+	return 0;
+}
+
+/**
+ * pmenu_delete - Delete a menu instance.
+ *
+ */
+
+void pmenu_delete(struct pmenu *menu)
+{
+	unsigned int i;
+
+	assert(menu->scr.sig == pb_pmenu_sig);
+	menu->scr.sig = pb_removed_sig;
+
+	for (i = item_count(menu->ncm); i; i--)
+		free_item(menu->items[i]);
+
+	free_menu(menu->ncm);
+	delwin(menu->scr.sub_ncw);
+	delwin(menu->scr.main_ncw);
+	talloc_free(menu);
+}

ui/ncurses/nc-scr.c
@@ -1,0 +1,129 @@
+/*
+ *  Copyright (C) 2009 Sony Computer Entertainment Inc.
+ *  Copyright 2009 Sony Corp.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <assert.h>
+#include <stdarg.h>
+
+#include "log/log.h"
+#include "talloc/talloc.h"
+
+#include "nc-scr.h"
+
+void nc_start(void)
+{
+	initscr();			/* Initialize ncurses. */
+	cbreak();			/* Disable line buffering. */
+	noecho();			/* Disable getch() echo. */
+	keypad(stdscr, TRUE);		/* Enable num keypad keys. */
+	nonl();				/* Disable new-line translation. */
+	intrflush(stdscr, FALSE);	/* Disable interrupt flush. */
+	curs_set(0);			/* Make cursor invisible */
+	nodelay(stdscr, TRUE);		/* Enable non-blocking getch() */
+	while (getch() != ERR)		/* flush stdin */
+		(void)0;
+}
+
+void nc_atexit(void)
+{
+	clear();
+	refresh();
+	endwin();
+}
+
+static void nc_scr_status_clear(struct nc_scr *scr)
+{
+	mvwhline(scr->main_ncw, LINES - nc_scr_pos_status, 0, ' ', COLS);
+}
+
+static void nc_scr_status_draw(struct nc_scr *scr)
+{
+	mvwaddstr(scr->main_ncw, LINES - nc_scr_pos_status, 1,
+		scr->frame.status);
+}
+
+void nc_scr_frame_draw(struct nc_scr *scr)
+{
+	DBGS("title '%s'\n", scr->frame.title);
+	DBGS("help '%s'\n", scr->frame.help);
+	DBGS("status '%s'\n", scr->frame.status);
+
+	mvwaddstr(scr->main_ncw, nc_scr_pos_title, 1, scr->frame.title);
+	mvwhline(scr->main_ncw, nc_scr_pos_title_sep, 1, ACS_HLINE, COLS - 2);
+
+	mvwhline(scr->main_ncw, LINES - nc_scr_pos_help_sep, 1, ACS_HLINE,
+		COLS - 2);
+	mvwaddstr(scr->main_ncw, LINES - nc_scr_pos_help, 1, scr->frame.help);
+	nc_scr_status_draw(scr);
+}
+
+void nc_scr_status_free(struct nc_scr *scr)
+{
+	talloc_free(scr->frame.status);
+	scr->frame.status = NULL;
+	nc_scr_status_clear(scr);
+}
+
+/**
+ * nc_scr_status_printf - Set the text of the scr status using sprintf.
+ * @scr: The scr to opperate on.
+ * @text: The status text.
+ *
+ * The caller is reponsible for calling scr_draw() to update the display.
+ */
+
+void nc_scr_status_printf(struct nc_scr *scr, const char *format, ...)
+{
+	va_list ap;
+
+	nc_scr_status_free(scr);
+
+	va_start(ap, format);
+	scr->frame.status = talloc_vasprintf(scr, format, ap);
+	va_end(ap);
+
+	nc_scr_status_draw(scr);
+	wrefresh(scr->main_ncw);
+}
+
+int nc_scr_init(struct nc_scr *scr, enum pb_nc_sig sig, int begin_x,
+	void *ui_ctx,
+	void (*process_key)(struct nc_scr *),
+	int (*post)(struct nc_scr *),
+	int (*unpost)(struct nc_scr *),
+	void (*resize)(struct nc_scr *))
+{
+	scr->sig = sig;
+	scr->ui_ctx = ui_ctx;
+	scr->process_key = process_key;
+	scr->post = post;
+	scr->unpost = unpost;
+	scr->resize = resize;
+
+	scr->main_ncw = newwin(LINES, COLS, 0, 0);
+
+	scr->sub_ncw = derwin(scr->main_ncw,
+		LINES - nc_scr_frame_lines,
+		COLS - 1 - begin_x,
+		nc_scr_pos_sub,
+		begin_x);
+
+	assert(scr->main_ncw);
+	assert(scr->sub_ncw);
+
+	return scr->main_ncw && scr->sub_ncw;
+}

ui/ncurses/nc-scr.h
@@ -1,0 +1,85 @@
+/*
+ *  Copyright (C) 2009 Sony Computer Entertainment Inc.
+ *  Copyright 2009 Sony Corp.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#if !defined(_PB_NC_SCR_H)
+#define _PB_NC_SCR_H
+
+#include <ncurses.h>
+
+#define DBG(fmt, args...) pb_log("DBG: " fmt, ## args)
+#define DBGS(fmt, args...) \
+	pb_log("DBG:%s:%d: " fmt, __func__, __LINE__, ## args)
+
+enum pb_nc_sig {
+	pb_cui_sig     = 111,
+	pb_pmenu_sig   = 222,
+	pb_item_sig    = 333,
+	pb_ked_sig     = 444,
+	pb_removed_sig = -555,
+};
+
+void nc_start(void);
+void nc_atexit(void);
+
+static inline void nc_flush_keys(void)
+{
+	while (getch() != ERR)
+		(void)0;
+}
+
+enum nc_scr_pos {
+	nc_scr_pos_title = 0,
+	nc_scr_pos_title_sep = 1,
+	nc_scr_pos_sub = 2,
+
+	nc_scr_pos_help_sep = 3,
+	nc_scr_pos_help = 2,
+	nc_scr_pos_status = 1,
+
+	nc_scr_frame_lines = 5,
+};
+
+struct nc_frame {
+	char *title;
+	char *help;
+	char *status;
+};
+
+struct nc_scr {
+	enum pb_nc_sig sig;
+	struct nc_frame frame;
+	WINDOW *main_ncw;
+	WINDOW *sub_ncw;
+	void *ui_ctx;
+	int (*post)(struct nc_scr *scr);
+	int (*unpost)(struct nc_scr *scr);
+	void (*process_key)(struct nc_scr *scr);
+	void (*resize)(struct nc_scr *scr);
+};
+
+int nc_scr_init(struct nc_scr *scr, enum pb_nc_sig sig, int begin_x,
+	void *ui_ctx,
+	void (*process_key)(struct nc_scr *),
+	int (*post)(struct nc_scr *),
+	int (*unpost)(struct nc_scr *),
+	void (*resize)(struct nc_scr *));
+void nc_scr_status_free(struct nc_scr *scr);
+void nc_scr_status_printf(struct nc_scr *scr, const char *format, ...);
+void nc_scr_frame_draw(struct nc_scr *scr);
+
+#endif

commit d09c19a79e3a18b1797695e9cc44956097b2235e

ncurses/nc-menu: Remove stray declaration
Signed-off-by: Geoff Levand <geoff@infradead.org>
Fixes: 863c6097 ("Add ncurses UI menu")
Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

ui/ncurses/nc-menu.h
@@ -61,7 +61,6 @@ void pmenu_item_insert(struct pmenu *menu, struct pmenu_item *item,
 	unsigned int index);
 void pmenu_item_add(struct pmenu *menu, struct pmenu_item *item,
 	unsigned int insert_pt);
-void pmenu_item_delete(struct pmenu_item *item);
 
 static inline struct pmenu_item *pmenu_item_from_arg(void *arg)
 {